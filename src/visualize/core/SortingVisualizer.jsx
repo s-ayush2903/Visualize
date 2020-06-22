1e501c97549d52046387c96b1b3e5cfb05560e4f
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2020-06-22 21:18:40 +0530

    random nums generated

---
+++ src/visualize/core/SortingVisualizer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './VisualizeUI.css'
+
+//Normal app stuff
+export default class SortingVisualizer extends React.Component {
+
+  //kinda injection
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      array: [],
+    };
+  }
+
+  //similar to overriding of onCreate()
+  componentDidMount() {
+    this.generateArray();   //do this so that screen doesn't appear blank when app is just loaded for the first time
+  }
+
+  generateArray() {
+    const array = [];
+    for (let i = 0; i < 100; i++)
+      array.push(pickRandNum(7, 432));
+    this.setState({array})
+  }
+
+  render() {
+    const { array } = this.state;
+
+    return (
+      <> {array.map((value, index) => (
+        <div className="array-bar" key={index}>
+          {value}
+        </div>
+      ))}
+      </>
+    );
+  }
+}
+
+//helper method
+function pickRandNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
